aa9fbcd8f959fb685957e0bbbf509d7c7e450187
sergiorbf <sbittencourt19@gmail.com>
2024-06-30 14:29:27 -0300

    save

---
+++ src/styles/global.ts
@@ -0,0 +1,9 @@
+export const globalTheme = {
+    colors: {
+        moss: '#424530',
+        brown: '#562926',
+        orange: '#c1632d',
+        beige: '#ffefcd',
+        caramel: '#a68e74'
+    }
+}
